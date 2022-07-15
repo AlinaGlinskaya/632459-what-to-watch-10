23b04e27fe1fe0e8ed07e9c39084d368158ce3d9
Alina <alinascarlett@mail.ru>
2022-07-15 20:00:10 +0300

    feat: create not-found-screen and import screens to app component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import MoviePageScreen from '../../pages/movie-page-screen/movie-page-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
 import {AppScreenProps} from '../../types/types';
 
 function App({promoFilm, films}: AppScreenProps): JSX.Element {
---
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,7 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+    <h1><b>404.</b><br /><small>Page not found</small></h1>
+  );
+}
+
+export default NotFoundScreen;
